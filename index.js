c648d9986e24670beb319020dca7dbd45a91b13f
Ernestovsosa1016 <l19141211@queretaro.tecnm.mx>
2024-12-11 02:00:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const multer = require('multer');
 const fs = require('fs');
-const path = require('path');
 const { google } = require('googleapis');
 const bodyParser = require('body-parser');
 const cors = require('cors');
